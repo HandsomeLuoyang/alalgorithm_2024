1573c6ae2a32aab5f240d3a841ec2f963c75e5c4
Hsluoyang <1605592159@qq.com>
2025-04-30 09:36:06 +0800

    update

--- leetcode/dynamic_programming/64-minimum-path-sum.cpp
+++ leetcode/dynamic_programming/64-minimum-path-sum.cpp
@@ -7,33 +7,17 @@ using namespace std;
 class Solution {
 public:
     int minPathSum(vector<vector<int>>& grid) {
-        int m = grid.size();
-        int n = grid[0].size();
-        int dp[m][n];
+        vector<vector<int> > dp(grid.size(), vector<int>(grid[0].size(), 0));
         dp[0][0] = grid[0][0];
-        for (int i = 1; i < m; ++i) {
-            dp[i][0] = dp[i-1][0] + grid[i][0];
-        }
-        for (int i = 1; i < n; ++i) {
-            dp[0][i] = dp[0][i-1] + grid[0][i];
-        }
-
-        for (int i = 1; i < m; ++i) {
-            for (int j = 1; j < n; ++j) {
-                dp[i][j] = min(dp[i-1][j], dp[i][j-1]) + grid[i][j];
+        for (int i = 0; i < grid.size(); i++) {
+            for (int j = 0; j < grid[0].size(); j++) {
+                if (i == 0 && j == 0) continue;
+                if (i == 0) dp[i][j] = dp[i][j-1] + grid[i][j];
+                else if (j == 0) dp[i][j] = dp[i-1][j] + grid[i][j];
+                else dp[i][j] = min(dp[i-1][j], dp[i][j-1])+grid[i][j];
             }
-
         }
-
-        for (int i = 0; i < m; ++i) {
-            for (int j = 0; j < n; ++j) {
-                std::cout << dp[i][j] << " ";
-            }
-            std::cout << std::endl; // 每打印完一行后换行
-        }
-
-        return dp[m-1][n-1];
-
+        return dp[grid.size()-1][grid[0].size()-1];
     }
 };
 
